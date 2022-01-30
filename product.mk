4ea132c48f42ad9f1a41ebae26366e85ed4452ec
Bruno Martins <bgcngm@gmail.com>
2022-01-30 21:08:41 +0000

    Don't force existence of MTG

--- product.mk
+++ product.mk
@@ -1,5 +1,5 @@
 # Google Apps
 ifeq ($(WITH_GMS), true)
 TARGET_IS_GROUPER := true
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
